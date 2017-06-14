57fcb922fa335a9a6bc46cd2399dc6e82afeb54d
johnny karlsson <johnny_karlsson75@hotmail.com>
2017-06-14 11:40:29 +0200

    Added Location Service och repository.

---
+++ MeasureMyPike/Repo/LocationRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MeasureMyPike.Models.Entity_Framework;
+
+namespace MeasureMyPike.Repo
+{
+    public class LocationRepository
+    {
+
+        public Location GetLocation(int id)
+        {            
+            using (var conn = new ModelContainer())
+            {
+                Location cc = conn.Location.FirstOrDefault(it => it.Id == id);
+                {
+                    if (cc != null)
+                    {
+                        return cc;
+                    }
+                    else return null;
+                }
+            }
+        }
+
+        public Location AddLocation(Location cc)
+        {
+            try
+            {
+                using (var conn = new ModelContainer())
+                {
+                    var c = conn.Location.Add(cc);
+                    conn.SaveChanges();
+                    return c;
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: better handling
+                Console.WriteLine(ex.GetType().FullName);
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public List<Location> GetAllLocations()
+        {
+            try
+            {
+                using (var conn = new ModelContainer())
+                {
+                    var value = conn.Location.ToList();
+                    return value;
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: better handling
+                Console.WriteLine(ex.GetType().FullName);
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public bool UpdateLocation(int id, string locationName, String coordinates)
+        {
+            using (var conn = new ModelContainer())
+            {
+                try
+                {
+                    Location o = conn.Location.FirstOrDefault(it => it.Id == id);
+                    o.Lake = locationName;
+                    o.Coordinates = coordinates;
+                    conn.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    // TODO: better handling
+                    Console.WriteLine(ex.GetType().FullName);
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ MeasureMyPike/Service/LocationService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MeasureMyPike;
+using MeasureMyPike.Repo;
+using MeasureMyPike.Models.Entity_Framework;
+
+
+namespace MeasureMyPike.Service
+{
+    public class LocationService
+    {
+
+        public Location AddLocation(String lake, String coordinates)
+        {
+            Location location = new Location
+            {                
+                Lake = lake,
+                Coordinates = coordinates                
+            };
+
+            LocationRepository dbconn = new LocationRepository();
+
+            return dbconn.AddLocation(location);
+        }
+
+        public Location GetLocation(int id)
+        {
+            LocationRepository dbconn = new LocationRepository();
+            return dbconn.GetLocation(id);
+        }
+
+        public List<Location> GetAllLocations()
+        {
+            LocationRepository dbconn = new LocationRepository();
+            return dbconn.GetAllLocations();
+        }
+
+        public bool UpdateLocation(int id, String name, String coordinates)
+        {
+            LocationRepository dbconn = new LocationRepository();
+
+            return dbconn.UpdateLocation(id, name, coordinates);
+        }
+
+
+    }
+}

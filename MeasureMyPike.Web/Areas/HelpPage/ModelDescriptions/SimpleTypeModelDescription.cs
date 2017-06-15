03d3c164b2eba06df8303117cac6a59befc4ad46
Fritjof Höst <fritjof.host@cgi.com>
2017-06-15 12:16:28 +0200

    ASP.NET single page application

---
+++ MeasureMyPike.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MeasureMyPike.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MeasureMyPike.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeasureMyPike.Web.MvcApplication" Language="C#" %>

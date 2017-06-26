5e9a7aee9b0f907860fe5d803345ef584cb090b0
Fritjof Höst <fritjof.host@cgi.com>
2017-06-26 17:01:56 +0200

    Fixat using i sql

--- Tests/MockData/Brands.sql
+++ Tests/MockData/Brands.sql
@@ -1,1 +1,3 @@
+use MeasureMyPike
+GO
 INSERT INTO Brand (Name) VALUES ('Abu Garcia'), ('Berkley'), ('Blue Fox'), ('Myran'), ('Rapala'), ('Salmo'), ('Savage Gear'), ('Storm'), ('Strike Pro'), ('Svartzonker'), ('Westin'), ('Wolfcreek Lures'), ('Zalt')

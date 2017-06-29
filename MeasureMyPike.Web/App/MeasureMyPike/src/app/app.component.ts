60eb575645ff43828624159caf2683862c32ee3f
Fritjof Höst <fritjof.host@cgi.com>
2017-06-29 16:32:48 +0200

    Lagt på flex och snyggat till allt. Påbörjat designen av en toolbar

--- MeasureMyPike.Web/App/MeasureMyPike/src/app/app.component.ts
+++ MeasureMyPike.Web/App/MeasureMyPike/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
     public title = 'MeasureMyPike';
+    public loggedIn: boolean = true;
 
     constructor() {
     }
--- MeasureMyPike.Web/App/MeasureMyPike/src/app/app.module.ts
+++ MeasureMyPike.Web/App/MeasureMyPike/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { MaterialModule, MdCheckboxModule, MdInputModule, MdButtonModule, MdTooltipModule, MdSidenavModule } from '@angular/material';
+import { MaterialModule, MdCheckboxModule, MdInputModule, MdButtonModule, MdTooltipModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations/';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -28,8 +28,7 @@ import { UserViewComponent } from './view/user-view/user-view.component';
         BrowserAnimationsModule,
         FlexLayoutModule,
         MdButtonModule,
-        MdTooltipModule,
-        MdSidenavModule
+        MdTooltipModule
     ],
     providers: [
         BrandService,
